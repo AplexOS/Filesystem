ddc6b7655c2408cc11d65f76c51cdc3854588dd7
chenfulin5 <chenfulin5@gmail.com>
2018-06-06 11:26:42 +0800

    Add : iptables

--- rootfs_test/usr/share/lighttpd/www/webpages/test_allled.sh
+++ rootfs_test/usr/share/lighttpd/www/webpages/test_allled.sh
@@ -3,22 +3,22 @@
 count=0
 while [ $count -le 10 ];
 do
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS1 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS3 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS5 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS7 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS9 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS11 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS13 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS2 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS4 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS6 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS8 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS10 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS12 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS14 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS16 &
-    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS15 &
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS1
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS2
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS3
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS4
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS5
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS6
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS7
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS8
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS9
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS10
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS11
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS12
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS13
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS14
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS15
+    echo 1234567890abcdefghqwertyuiopdasdk > /dev/ttyS16
     echo 1 > /dev/DO0
     echo 1 > /dev/DO1
     echo 1 > /dev/DO2
