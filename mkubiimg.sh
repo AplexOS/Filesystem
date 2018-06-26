be0160c6c834181a9571202e38de0e1eda86461a
chenfulin5 <chenfulin5@gmail.com>
2018-06-26 11:22:14 +0800

    git add iproute2

--- mkubiimg.sh
+++ mkubiimg.sh
@@ -2,7 +2,7 @@
 
 #sudo mkfs.ubifs  -q -r rootfs -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
 #sudo mkfs.ubifs  -q -r rootfs_20180110 -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
-sudo mkfs.ubifs  -q -r rootfs_20180508 -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
+sudo mkfs.ubifs  -q -r rootfs_iproute -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
 echo mkfs.ubifs over!
 sudo ubinize -o ubi.img -m 2048 -p 128KiB ubinize.cfg
 echo ubinize over!
---
+++ rootfs_iproute/usr/share/lighttpd/www/webpages/backend.php
@@ -0,0 +1,55 @@
+<?php header("Access-Control-Allow-Origin: *") ?>
+<?php
+    include 'functions.php';
+
+    if ($_FILES["fileToUpload"]["error"] > 0)
+    {
+        $json_array["status"] = "error";
+        $json_array["result"] = "Return Code: " . $_FILES["fileToUpload"]["error"] . "<br />";
+    }
+    else
+    {
+        $json_array["name"] = $_FILES["fileToUpload"]["name"];
+        $json_array["size"] = $_FILES["fileToUpload"]["size"];
+
+        /*
+        if (file_exists("uploads/" . $_FILES["fileToUpload"]["name"]))
+        {
+            $json_array["status"] = "error";
+            $json_array["result"] = $_FILES["fileToUpload"]["name"] . " already exists. ";
+        }
+         */
+        move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],
+            "uploads/" . $_FILES["fileToUpload"]["name"]);
+
+        $data = json_decode($_POST["data"], true);
+        $categories = $data["categories"];
+        $type = $data["type"];
+
+        if ($categories == "upload") {
+
+            $json_array["type"] = $type;
+            if ($type == "MLO") {
+                $json_array["status"] = "ok";
+                $json_array["cmd"] = $MiniOS->configs[$type]["cmd"];
+                $json_array["result"] = shell_exec($MiniOS->configs[$type]["cmd"]);
+            } else if ($type == "u-boot.img") {
+                $json_array["cmd"] = $MiniOS->configs[$type]["cmd"];
+                $json_array["result"] = shell_exec($MiniOS->configs[$type]["cmd"]);
+                $json_array["status"] = "ok";
+            } else if ($type == "uImage") {
+                $json_array["cmd"] = $MiniOS->configs[$type]["cmd"];
+                $json_array["result"] = shell_exec($MiniOS->configs[$type]["cmd"]);
+                $json_array["status"] = "ok";
+            } else if ($type == "ubi.img") {
+                $json_array["cmd"] = $MiniOS->configs[$type]["cmd"];
+                $json_array["result"] = shell_exec($MiniOS->configs[$type]["cmd"]);
+                $json_array["status"] = "ok";
+            } else {
+                $json_array["status"] = "error";
+            }
+
+        }
+    }
+    echo json_encode($json_array);
+?>
