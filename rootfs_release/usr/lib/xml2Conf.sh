39e3a053f813100f6c8bdd04350214bcfa00e84f
chenfulin5 <chenfulin5@gmail.com>
2018-07-04 14:05:50 +0800

    CMI_AT101 filesystem initw

---
+++ rootfs_release/usr/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/usr/lib"
+XML2_LIBS="-lxml2 -L/home/cmi_at101/buildroot-2017.02.5/output/host/usr/arm-buildroot-linux-gnueabihf/sysroot/usr/lib -lz       -lm "
+XML2_INCLUDEDIR="-I/usr/include/libxml2"
+MODULE_VERSION="xml2-2.9.4"
+
