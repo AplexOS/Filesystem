e1284707bfbf43056b559f13c7c59b60a041987b
chenfulin5 <chenfulin5@gmail.com>
2019-04-11 09:28:49 +0800

    add wireless file

--- mkubiimg.sh
+++ mkubiimg.sh
@@ -2,9 +2,11 @@
 
 #sudo mkfs.ubifs  -q -r rootfs -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
 #sudo mkfs.ubifs  -q -r rootfs_20180110 -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
-sudo mkfs.ubifs  -q -r rootfs_iproute -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
+#sudo mkfs.ubifs  -q -r rootfs_iproute -m 2048 -e 126976 -c 2047 -o  ubifs.img -F
+sudo mkfs.ubifs  -q -r rootfs_iproute -m 2048 -e 124KiB -c 1680 -o  ubifs.img -F
 echo mkfs.ubifs over!
-sudo ubinize -o ubi.img -m 2048 -p 128KiB ubinize.cfg
+#sudo ubinize -o ubi.img -m 2048 -p 124KiB ubinize.cfg
+sudo ubinize -o ubi.img -m 2048 -p 128KiB -s 512 -O 2048 ubinize.cfg
 echo ubinize over!
 
 sync
